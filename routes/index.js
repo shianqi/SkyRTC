7dd4b3f57f0e800ef10aa95f19d5d2d78bd66a42
killer <shianqi@imudges.com>
2016-10-24 17:31:25 +0800

    登陆简单完成

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,49 @@
 var express = require('express');
+var session = require('express-session');
 var router = express.Router();
 var path = require('path');
 
+router.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: false,
+    cookie:{
+        maxAge: 1000*60*10 //cookie有效时间10min
+    }
+}));
+
 router.get('/', function(req, res) {
-    res.sendfile('index.html');
+    res.render('index');
 });
 
+router.get('/room', function(req, res) {
+    if(haveLogined(req.session.user)){
+        res.render('room');
+    }else{
+        res.redirect('/');
+    }
+});
+
+router.post('/login', function (req, res) {
+    var user = {
+        username:'admin',
+        password:'admin'
+    };
+    if(req.body.username==user.username&&req.body.password==user.password){
+        req.session.user = user;
+        res.redirect('/room');
+    }else{
+        res.redirect('/');
+    }
+});
+
+//判断用户是否登陆
+var haveLogined = function (user) {
+    if(typeof(user) == "undefined"){
+        return false;
+    }else{
+        return true;
+    }
+};
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var fs = require('fs');
 var path = require("path");
 var app = express();
-
+var bodyParser = require('body-parser');
 var routes = require('./routes/index');
-var room = require('./routes/room');
+var cookieParser = require('cookie-parser');
 
 var key = fs.readFileSync('keys/newkey.pem');
 var cert = fs.readFileSync('keys/cert.pem');
+//https密钥
 var https_options = {
     key: key,
     cert: cert
@@ -18,42 +19,60 @@ var SkyRTC = require('skyrtc').listen(server);
 var port = process.env.PORT || 443;
 server.listen(port);
 
+//配置请求头
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    next();
+});
+
+//配置公共资源文件夹
 app.use(express.static(path.join(__dirname, 'public')));
+
+//配置post请求
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//配置cookie
+app.use(cookieParser());
+
+//配置前端模版为ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //加载主页模块
 app.use('/', routes);
-app.use('/room', room);
 
 SkyRTC.rtc.on('new_connect', function(socket) {
-	console.log('创建新连接');
+    console.log('创建新连接');
 });
 
 SkyRTC.rtc.on('remove_peer', function(socketId) {
-	console.log(socketId + "用户离开");
+    console.log(socketId + "用户离开");
 });
 
 SkyRTC.rtc.on('new_peer', function(socket, room) {
-	console.log("新用户" + socket.id + "加入房间" + room);
+    console.log("新用户" + socket.id + "加入房间" + room);
 });
 
 SkyRTC.rtc.on('socket_message', function(socket, msg) {
-	console.log("接收到来自" + socket.id + "的新消息：" + msg);
+    console.log("接收到来自" + socket.id + "的新消息：" + msg);
 });
 
 SkyRTC.rtc.on('ice_candidate', function(socket, ice_candidate) {
-	console.log("接收到来自" + socket.id + "的ICE Candidate");
+    console.log("接收到来自" + socket.id + "的ICE Candidate");
 });
 
 SkyRTC.rtc.on('offer', function(socket, offer) {
-	console.log("接收到来自" + socket.id + "的Offer");
+    console.log("接收到来自" + socket.id + "的Offer");
 });
 
 SkyRTC.rtc.on('answer', function(socket, answer) {
-	console.log("接收到来自" + socket.id + "的Answer");
+    console.log("接收到来自" + socket.id + "的Answer");
 });
 
 SkyRTC.rtc.on('error', function(error) {
-	console.log("发生错误：" + error.message);
+    console.log("发生错误：" + error.message);
 });
